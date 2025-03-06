3e604469fca804b8f24a1e9696a2e8bf28972a5d
Jasper Day <jasper@linearized.xyz>
2025-03-06 11:04:55 +0000

    Add fish scripts to set environment variables for WSL

---
+++ run_linux.fish
@@ -0,0 +1,6 @@
+#! /usr/bin/fish
+
+set -x FG_BINARY '/home/jasper/jsb/FlightGear-2024.2.0-x86_64.AppImage'
+set -x PX4_FG_ROOT '/usr/share/games/flightgear'
+set -x FG_MODELS_DIR './models'
+set -x PX4_FG_ROOT_WINDOWS '/usr/share/games/flightgear'
---
+++ run_windows.fish
@@ -0,0 +1,6 @@
+#! /usr/bin/fish
+
+set -x FG_BINARY '/mnt/c/Program\ Files/FlightGear\ 2024.1/bin/fgfs.exe'
+set -x PX4_FG_ROOT '/mnt/d/FlightGear/FlightGear_2024.1/data'
+set -x FG_MODELS_DIR 'D:/FlightGear/Models'
+set -x PX4_FG_ROOT_WINDOWS 'D:/FlightGear/FlightGear_2024.1/data'
